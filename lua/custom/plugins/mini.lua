a0a4881ee48f0205b7f9f1e92a9efc87227b1824
demorome <demoromestuff@gmail.com>
2026-08-30 12:26:01 -0300

    feat: improve mini.statusline
    Removed redundant/useless info and added highlighting for filename

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -33,12 +33,59 @@ require('mini.ai').setup {
 -- - sr)'  - [S]urround [R]eplace [)] [']
 require('mini.surround').setup()
 
--- Simple and easy statusline.
---  You could remove this setup call if you don't like it,
---  and try some other statusline plugin
 local statusline = require 'mini.statusline'
--- Set `use_icons` to true if you have a Nerd Font
-statusline.setup { use_icons = vim.g.have_nerd_font }
+statusline.setup {
+  use_icons = vim.g.have_nerd_font,
+  content = {
+    --content for active window
+    active = function()
+      local mode, mode_hl = MiniStatusline.section_mode { trunc_width = 120 }
+      local git = MiniStatusline.section_git { trunc_width = 40 }
+      local diff = MiniStatusline.section_diff { trunc_width = 75 }
+      local diagnostics = MiniStatusline.section_diagnostics {
+        trunc_width = 75,
+        signs = { ERROR = '!', WARN = '?', INFO = '@', HINT = '*' },
+      }
+      -- local lsp           = MiniStatusline.section_lsp({ trunc_width = 75 })
+      --local filename = MiniStatusline.section_filename { trunc_width = 140 }
+      local getFileDirectory = function()
+        if !MiniStatusline.is_truncated(120) then
+          -- Show full directory, with home dir shortened to '~/'
+          return vim.fn.expand '%:p:~:h' .. '/'
+        else
+          -- Show relative directory.
+          return vim.fn.expand '%:h' .. '/'
+        end
+      end
+      -- local fileinfo = MiniStatusline.section_fileinfo { trunc_width = 120 }
+      local fileInfo = function()
+          -- Return empty string if truncated or buffer is not normal.
+          if MiniStatusline.is_truncated(120) or vim.bo.buftype ~= '' then
+        return
+        end
+          local encoding = vim.bo.fileencoding or vim.bo.encoding
+          local format = vim.bo.fileformat
+          return string.format("%s[%s]", encoding, format)
+      end
+      local location = MiniStatusline.section_location { trunc_width = 75 }
+      local search = MiniStatusline.section_searchcount { trunc_width = 75 }
+
+      return MiniStatusline.combine_groups {
+        { hl = mode_hl, strings = { string.upper(mode) } },
+        { hl = 'MiniStatuslineDevinfo', strings = { git, diff, diagnostics } },
+        '%<', -- Mark general truncate point
+        -- Show file's directory in grayed-out text.
+        { hl = 'MiniStatuslineFilename', strings = { getFileDirectory() } },
+        -- Show filename in brighter text.
+        -- TODO: Convince plugin author to allow overriding spacing here, since two spaces are currently introduced.
+        { hl = 'MiniStatuslineFileinfo', strings = { vim.fn.expand '%:t' } },
+        '%=', -- End left alignment
+        { hl = 'MiniStatuslineFileinfo', strings = { fileInfo() } },
+        { hl = mode_hl, strings = { search, location } },
+      }
+    end,
+  },
+}
 
 -- You can configure sections in the statusline by overriding their
 -- default behavior. For example, here we set the section for
@@ -48,8 +95,5 @@ statusline.section_location = function() return '%2l:%-2v' end
 
 -- ... and there is more!
 --  Check out: https://github.com/nvim-mini/mini.nvim
-
---require('mini.files').setup()
-
 
 -- vim: ts=2 sts=2 sw=2 et
