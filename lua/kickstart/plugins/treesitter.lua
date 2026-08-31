85cff047b2c14054e2f9e245c3834a0915ef87d1
demorome <demoromestuff@gmail.com>
2026-08-31 18:52:10 -0300

    feat: add treesitter-context for sticky top context view (class, function)

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -40,7 +40,7 @@ vim.api.nvim_create_autocmd('FileType', {
 
     local language = vim.treesitter.language.get_lang(filetype)
     if not language then
-      print("Treesitter.lua: Failed to find language for filetype " .. filetype)
+      print('Treesitter.lua: Failed to find language for filetype ' .. filetype)
       return
     end
 
@@ -58,5 +58,23 @@ vim.api.nvim_create_autocmd('FileType', {
     end
   end,
 })
+
+-- Show function/class context as a sticky reminder on the top.
+vim.pack.add { gh 'nvim-treesitter/nvim-treesitter-context' }
+require('treesitter-context').setup {
+  enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+  multiwindow = false, -- Enable multiwindow support.
+  max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+  min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+  line_numbers = true,
+  multiline_threshold = 20, -- Maximum number of lines to show for a single context
+  trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+  mode = 'cursor', -- Line used to calculate context. Choices: 'cursor', 'topline'
+  -- Separator between context and content. Should be a single character string, like '-'.
+  -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+  separator = nil,
+  zindex = 20, -- The Z-index of the context window
+  on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+}
 
 -- vim: ts=2 sts=2 sw=2 et
