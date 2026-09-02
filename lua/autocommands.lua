13bf3fe489458b993850dc690c75ac88929102e8
demorome <demoromestuff@gmail.com>
2026-09-02 11:16:49 -0300

    feat(autocommand): add auto-terminal insert autocmd

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -1,13 +1,30 @@
+local gr = vim.api.nvim_create_augroup('BasicAutocommands', {})
+
 -- Update the current file when entering Terminal mode,
 -- since I've probably already forgotten to save the file.
 vim.api.nvim_create_autocmd({ 'TermEnter' }, {
+  group = gr,
   command = 'silent! update',
+})
+
+-- Credits to Mini.Basics: https://github.com/nvim-mini/mini.basics/blob/main/lua/mini/basics.lua
+local start_terminal_insert = vim.schedule_wrap(function(data)
+  -- Try to start terminal mode only if target terminal is current
+  if not (vim.api.nvim_get_current_buf() == data.buf and vim.bo.buftype == 'terminal') then return end
+  vim.cmd 'startinsert'
+end)
+vim.api.nvim_create_autocmd('TermOpen', {
+  group = gr,
+  pattern = { 'term://*' },
+  callback = start_terminal_insert,
+  desc = 'Start builtin terminal in Insert mode',
 })
 
 -- Auto-save file when running project commands.
 -- TODO: Save ALL files in open buffers!
 local projectCommands = { 'Dotnet', 'make', 'Git' }
 vim.api.nvim_create_autocmd('CmdlineLeave', {
+  group = gr,
   callback = function()
     local cmd = vim.fn.getcmdline()
     cmd = vim.fn.fullcommand(cmd)
@@ -19,6 +36,7 @@ vim.api.nvim_create_autocmd('CmdlineLeave', {
 
 -- turn on spell check for markdown and text file
 vim.api.nvim_create_autocmd('BufEnter', {
+  group = gr,
   pattern = { '*.md' },
   callback = function() vim.opt_local.spell = true end,
 })
