4b9f737e9931767aa89efbd3b1455081abc4033c
demorome <demoromestuff@gmail.com>
2026-09-08 21:24:08 -0300

    tweak: disable guess-ident, since I probably don't need it

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,6 +3,7 @@
 -- Purely visual tweaks.
 require 'custom.plugins.theme' -- set visual theme.
 require 'kickstart.plugins.todo-comments' -- highlight TODO etc. comments
+require 'kickstart.plugins.indent_line' -- add indent guides.
 
 -- Enable built-in :UndoTree.
 -- This is useful to view different 'branches' of changes, if we accidentally clobber our undo/redo history.
@@ -16,7 +17,7 @@ require 'kickstart.plugins.telescope' -- fuzzy-finding tools
 require 'custom.plugins.lspconfig'
 
 require 'kickstart.plugins.conform'
-require 'custom.plugins.autocomplete'
+require 'custom.plugins.autocomplete' -- TODO: Migrate to native autocomplete once it supports multiple sources (snippets, etc.)
 require 'kickstart.plugins.treesitter'
 
 require 'custom.plugins.debug'
@@ -29,8 +30,7 @@ require 'custom.plugins.nvim-orgmode' -- TODO: Replace with a simple markdown al
 require 'custom.plugins.lazygit' -- TODO: Replace with futigive?
 require 'custom.plugins.bookmarks'
 
-require 'kickstart.plugins.guess-indent'
-require 'kickstart.plugins.indent_line'
+-- require 'kickstart.plugins.guess-indent'
 require 'kickstart.plugins.autopairs'
 require 'custom.plugins.better_quick_fix'
 
